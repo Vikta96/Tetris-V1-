e7ae35eb1e55eafbdd8f93d7db839a73fc531c35
Anandm894 <anandbm1995@gmail.com>
2023-12-21 17:24:59 +0530

    v1 files to be commit

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "tetris-23" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-southeast-2"
   }
